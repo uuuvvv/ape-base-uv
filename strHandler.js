cd3c21d813511901b37fa2f4319e9758afa6070b
uuuvvv <jyzhl11@163.com>
2022-04-16 15:46:41 +0800

    更改方法

--- strHandler.js
+++ strHandler.js
@@ -1,13 +1,19 @@
 // 返回字符串中字符最多的字符以及数量
 let str = 'dshfsdjflsdjflsjf'
 let o = {}
+// 整理字符串
 for (let i = 0; i < str.length; i++) {
   let item = str[i];
   o[item] = o[item] + 1 || 1;
 }
+let t = str[0];
 let n = 0;
 // 取出最大值
 for (let k in o) {
-  n = o[k] > n ? o[k] : n
+  // n = o[k] > n ? o[k] : n
+  if (o[k] > n) {
+    t = k;
+    n = o[k];
+  }
 }
-console.log(n, o);
+console.log(t, n);
