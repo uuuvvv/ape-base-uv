c433e687836d2ab33f9a8972d3ea4d4d9eea125d
uuuvvv <jyzhl11@163.com>
2022-06-13 23:59:40 +0800

    冒泡排序

--- jsAlgorithm.js
+++ jsAlgorithm.js
@@ -2,5 +2,15 @@
 
 // 冒泡排序
 function maoPo() {
-  
+  let a = [1, 2, 5, 8, 9, 45, 12, 65];
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i+1; j < a.length; j++) {
+      if (a[i] > a[j]) {
+        let c = a[i];
+        a[i] = a[j];
+        a[j] = c;
+      }
+    }    
+  }
+  console.log(a);
 }
