267ee9c643134a072797b34fcc2838872068b636
it-zhang <jyzhl11@163.com>
2022-08-02 17:13:21 +0800

    增加了examples文件夹配置，以及更新最新笔记

---
+++ Z-学习笔记/Ts学习笔记/examples/typescriptTest.ts
@@ -0,0 +1,22 @@
+
+// symbol 类型的值不是字面量类型，不能作为计算属性名（个人认为：计算属性名即变量的值作为对象属性名。）
+const x:unique symbol = Symbol();
+const y:symbol = Symbol();
+// 接口
+interface Foo {
+    [x]:string;
+    // [y]:string;//错误。
+}
+
+// ts 中只允许使用 const 声明 和 readonly 属性声明来定义 ‘ unique symbol ’类型的值。
+const a:unique symbol  = Symbol();
+
+interface withUniqueSymbol {
+    // 必须使用readonly 修饰符
+    readonly b:unique symbol;
+}
+// class 类
+class C {
+    // 必须使用 static 和 readonly 修饰符
+    static readonly c: unique symbol = Symbol();
+}
